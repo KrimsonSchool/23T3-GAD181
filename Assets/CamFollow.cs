26bcdcaf4d2ef9e66cb6805ba7c69d580dd49971
SAE1\1027859 <1027859@student.sae.edu.au>
2023-09-18 13:22:56 +1000

    upgrades to the race game

---
+++ Assets/CamFollow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public GameObject toFollow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = toFollow.transform.position;
+
+        transform.rotation = toFollow.transform.rotation;
+    }
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
-    public float Rotspeed;
+    public float rotSpeed;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        //transform.position += transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
 
-        transform.Rotate(transform.up, Input.GetAxis("Horizontal") * Rotspeed * Time.deltaTime);
+        rb.AddForce(transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime);
+
+        transform.Rotate(transform.up, Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime);
     }
 }
