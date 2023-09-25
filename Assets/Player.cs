70e0d7721bb7f7d629dd32b8a2b542e5fece4967
SAE1\1027859 <1027859@student.sae.edu.au>
2023-09-25 10:19:19 +1000

    added multiplayer + improved track

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public int playerNo;
     public float speed;
     public float rotSpeed;
     Rigidbody rb;
+
+    float forwardV;
+    float rightwardV;
+
+    public KeyCode[] forwardKey;
+    public KeyCode[] backwardKey;
+    public KeyCode[] rightKey;
+    public KeyCode[] leftKey;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +26,36 @@ public class Player : MonoBehaviour
     void Update()
     {
         //transform.position += transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        rb.AddForce(transform.forward * forwardV * speed * Time.deltaTime);
 
-        rb.AddForce(transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        transform.Rotate(transform.up, rightwardV * rotSpeed * Time.deltaTime);
 
-        transform.Rotate(transform.up, Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime);
+
+
+        if (Input.GetKey(forwardKey[playerNo]))
+        {
+            forwardV = 1;
+        }
+        else if (Input.GetKey(backwardKey[playerNo]))
+        {
+            forwardV = -1;
+        }
+        else
+        {
+            forwardV = 0;
+        }
+
+        if (Input.GetKey(rightKey[playerNo]))
+        {
+            rightwardV = 1;
+        }
+        else if (Input.GetKey(leftKey[playerNo]))
+        {
+            rightwardV = -1;
+        }
+        else
+        {
+            rightwardV = 0;
+        }
     }
 }
