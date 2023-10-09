255a7627c28919f67e5abf0c73f634142864b7d4
SAE1\1027859 <1027859@student.sae.edu.au>
2023-10-09 10:24:36 +1000

    Update to game picker script

---
+++ Assets/GamePicker.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamePicker : MonoBehaviour
+{
+    public string[] games;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Game1()
+    {
+        SceneManager.LoadScene(games[0]);
+    }
+    public void Game2()
+    {
+        SceneManager.LoadScene(games[1]);
+
+    }
+    public void Game3()
+    {
+        SceneManager.LoadScene(games[2]);
+
+    }
+    public void Game4()
+    {
+        SceneManager.LoadScene(games[3]);
+
+    }
+}
