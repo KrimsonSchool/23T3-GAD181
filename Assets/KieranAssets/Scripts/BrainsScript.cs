9d6759119aa1a61fedcedfa49ef2b8c2b0430cf9
TORBJORNGAMINGP\Windows PC <1027606@student.sae.edu.au>
2023-10-11 12:38:57 +1000

    - Software issues fixed (Visual studio not respoding) - Reinstalled visual studio - 3 scripts made for game manager,  player controls and brain tem.

---
+++ Assets/KieranAssets/Scripts/BrainsScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrainsScript : MonoBehaviour
+{
+    public bool playerOneFood = false;
+    public bool playerTwoFood = false;
+    public bool playerThreeFood = false;
+    public bool playerFourFood = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerOneFood)
+        {
+
+        }
+
+
+    }
+}
