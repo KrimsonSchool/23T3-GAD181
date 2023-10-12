611440ecf5dede58b5c3f7ce4fc6c3df59d07895
TORBJORNGAMINGP\Windows PC <1027606@student.sae.edu.au>
2023-10-12 16:30:20 +1000

    Brain Slammer - Working on eating mechanics and adding button input for each player. - Added some place holders for UI

--- Assets/KieranAssets/Scripts/BrainsScript.cs
+++ Assets/KieranAssets/Scripts/BrainsScript.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class BrainsScript : MonoBehaviour
 {
-    public GameObject playerOneFood;
-    public GameObject playerTwoFood;
-    public GameObject playerThreeFood;
-    public GameObject playerFourFood;
-    public int FoodAmountOne = 10;
-    public bool isFoodEaten = false;
+    public GameObject playerOneFood; // This is a reference to the gameObject in the scene of playerOnesfood
+    public GameObject playerTwoFood; // This is a reference to the gameObject in the scene of playerTwosfood
+    public GameObject playerThreeFood; // This is a reference to the gameObject in the scene of playerThreesfood
+    public GameObject playerFourFood; // This is a reference to the gameObject in the scene of playerFoursfood
+    public int FoodAmountOne = 10; // This holds the amount of food left to eat in 1 brain.
+    public int FoodAmountTwo = 10; // This holds the amount of food left to eat in 1 brain.
+    public int FoodAmountThree = 10; // This holds the amount of food left to eat in 1 brain.
+    public int FoodAmountFour = 10; // This holds the amount of food left to eat in 1 brain.
+    public bool isPoneFoodEaten = false; // This checks is the food has been eaten and it initalise is set to false
+    public bool isPtwoFoodEaten = false; // This checks is the food has been eaten and it initalise is set to false
+    public bool isPthreeFoodEaten = false; // This checks is the food has been eaten and it initalise is set to false
+    public bool isPfourFoodEaten = false; // This checks is the food has been eaten and it initalise is set to false
 
 
     // Start is called before the first frame update
@@ -21,23 +27,68 @@ public class BrainsScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PlayersEating(); // This call the 
         
-        if(FoodAmountOne == 0 && isFoodEaten)
+    }
+    void PlayersEating()
+    {
+        if (FoodAmountOne == 0 && isPoneFoodEaten == false)
         {
-            
+
             playerOneFood.SetActive(false);
             Debug.Log("Food transfer");
             FoodAmountOne += 10;
             playerOneFood.SetActive(true);
             Debug.Log("Food Delivered");
-            isFoodEaten = true;
+            isPoneFoodEaten = true;
+        }
+        else if (isPoneFoodEaten == true)
+        {
+            isPoneFoodEaten = false;
+
         }
 
-        if(isFoodEaten == true)
+        if (FoodAmountTwo == 0 && isPtwoFoodEaten == false)
         {
-            isFoodEaten = false;
-            
+            playerTwoFood.SetActive(false);
+            Debug.Log("Food transfer");
+            FoodAmountTwo += 10;
+            playerTwoFood.SetActive(true);
+            Debug.Log("Food Delivered");
+            isPtwoFoodEaten = true;
         }
-        
+        else if (isPtwoFoodEaten == true)
+        {
+            isPtwoFoodEaten = false;
+        }
+
+        if (FoodAmountThree == 0 && isPthreeFoodEaten == false)
+        {
+            playerThreeFood.SetActive(false);
+            Debug.Log("Food transfer");
+            FoodAmountThree += 10;
+            playerThreeFood.SetActive(true);
+            Debug.Log("Food Delivered");
+            isPthreeFoodEaten = true;
+        }
+        else if (isPthreeFoodEaten == true)
+        {
+            isPthreeFoodEaten = false;
+        }
+
+        if (FoodAmountFour == 0 && isPfourFoodEaten == false)
+        {
+            playerFourFood.SetActive(false);
+            Debug.Log("Food transfer");
+            FoodAmountFour += 10;
+            playerFourFood.SetActive(true);
+            Debug.Log("Food Delivered");
+            isPfourFoodEaten = true;
+        }
+        else if (isPfourFoodEaten == true)
+        {
+            isPfourFoodEaten = false;
+        }
     }
+
 }
--- Assets/KieranAssets/Scripts/PlayerController.cs
+++ Assets/KieranAssets/Scripts/PlayerController.cs
@@ -29,11 +29,47 @@ public class Playercontroller : MonoBehaviour
                 foodAccess.FoodAmountOne -= 1;
                 Debug.Log("KEEP EATING!!!");
             }
-            
             else if (foodAccess.FoodAmountOne == 0)
             {
                 Debug.Log("All food is eaten");
             }
         }
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            if (foodAccess.FoodAmountTwo >= 1 && foodAccess.FoodAmountTwo <= 10)
+            {
+                foodAccess.FoodAmountTwo -= 1;
+                Debug.Log("KEEP EATING!!!");
+            }
+            else if (foodAccess.FoodAmountTwo == 0)
+            {
+                Debug.Log("All food is eaten");
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightAlt))
+        {
+            if (foodAccess.FoodAmountThree >= 1 && foodAccess.FoodAmountThree <= 10)
+            {
+                foodAccess.FoodAmountThree -= 1;
+                Debug.Log("KEEP EATING!!!");
+            }
+            else if (foodAccess.FoodAmountThree == 0)
+            {
+                Debug.Log("All food is eaten");
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightControl))
+        {
+            if (foodAccess.FoodAmountFour >= 1 && foodAccess.FoodAmountFour <= 10)
+            {
+                foodAccess.FoodAmountFour -= 1;
+                Debug.Log("KEEP EATING!!!");
+            }
+            else if (foodAccess.FoodAmountFour == 0)
+            {
+                Debug.Log("All food is eaten");
+            }
+        }
+
     }
 }
