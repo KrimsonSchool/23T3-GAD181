15696da56e1b0759b1354f059081b1a7633db4a2
SAE1\1027859 <1027859@student.sae.edu.au>
2023-10-23 09:51:53 +1000

    added scoring system

---
+++ Assets/GlobalScore.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalScore : MonoBehaviour
+{
+    public int player1Score;
+    public int player2Score;
+    public int player3Score;
+    public int player4Score;
+
+    public TMPro.TextMeshProUGUI scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player1Score = PlayerPrefs.GetInt("player1Score");
+        player2Score = PlayerPrefs.GetInt("player2Score");
+        player3Score = PlayerPrefs.GetInt("player3Score");
+        player4Score = PlayerPrefs.GetInt("player4Score");
+
+        PlayerPrefs.SetInt("player1Score", PlayerPrefs.GetInt("player1Score") + 50);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "· Player 1: " + player1Score + "\n\n· Player 2: " + player2Score + "\n\n· Player 3:" + player3Score + "\n\n· Player 4:" + player4Score + "\n\n";
+    }
+}
