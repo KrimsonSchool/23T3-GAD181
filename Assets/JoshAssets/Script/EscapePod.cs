de15a68bbc3af40e49d27a2ec0756712b429275b
SAE1\1029946 <1029946@student.sae.edu.au>
2023-10-23 13:37:14 +1000

    cam working

--- Assets/JoshAssets/Script/EscapePod.cs
+++ Assets/JoshAssets/Script/EscapePod.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class EscapePod : MonoBehaviour
 {
     public PlayerMovement playerMovement;
-    public GameObject uiWinPrompt;
+    public GameObject uiPlayerOneWinPrompt;
+    public GameObject uiPlayerTwoWinPrompt;
+    public GameObject uiPlayerThreeWinPrompt;
+    public GameObject uiPlayerFourWinPrompt;
     // Start is called before the first frame update
     void Start()
     {
-        
+        uiPlayerOneWinPrompt.SetActive(false);
+        uiPlayerTwoWinPrompt.SetActive(false);
+        uiPlayerThreeWinPrompt.SetActive(false);
+        uiPlayerFourWinPrompt.SetActive(false);
     }
     private void OnTriggerEnter(Collider Player)
     {
@@ -17,26 +23,57 @@ public class EscapePod : MonoBehaviour
         if (Player.tag == "PlayerOne")
         {
             Debug.Log("Player One Wins");
+            uiPlayerOneWinPrompt.SetActive(true);
+            StartCoroutine(PlayerOneWinner());
         }
 
         if (Player.tag == "PlayerTwo")
         {
             Debug.Log("Player Two Wins");
+            uiPlayerTwoWinPrompt.SetActive(true);
+            StartCoroutine(PlayerTwoWinner());
         }
 
         if (Player.tag == "PlayerThree")
         {
             Debug.Log("Player Three Wins");
+            uiPlayerThreeWinPrompt.SetActive(true);
+            StartCoroutine(PlayerThreeWinner());            
         }
 
         if (Player.tag == "PlayerFour")
         {
             Debug.Log("Player Four Wins");
+            uiPlayerFourWinPrompt.SetActive(true);
+            StartCoroutine(PlayerFourWinner());
         }
         // disable movement of all characters
         playerMovement.enabled = false;
     }
 
+    public IEnumerator PlayerOneWinner()
+    {
+        yield return new WaitForSeconds(10f);
+        uiPlayerOneWinPrompt.SetActive(false);
+    }
+
+    public IEnumerator PlayerTwoWinner()
+    {
+        yield return new WaitForSeconds(10f);
+        uiPlayerTwoWinPrompt.SetActive(false);
+    }
+
+    public IEnumerator PlayerThreeWinner()
+    {
+        yield return new WaitForSeconds(10f);
+        uiPlayerThreeWinPrompt.SetActive(false);
+    }
+
+    public IEnumerator PlayerFourWinner()
+    {
+        yield return new WaitForSeconds(10f);
+        uiPlayerFourWinPrompt.SetActive(false);
+    }
     // Update is called once per frame
     void Update()
     {
