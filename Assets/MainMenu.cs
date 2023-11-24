80c8148fed4c0166fb67c5dd8e38024e6ae114fe
JAIS_PC\jaivd <jaivda111@gmail.com>
2023-11-24 18:42:32 +1000

    Commented main menu script

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class EmmersonsUIStolenFromJaiDeleteThisLater : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
     public Slider roundsSlider;
     public TMPro.TextMeshProUGUI sliderText;
@@ -17,27 +17,36 @@ public class EmmersonsUIStolenFromJaiDeleteThisLater : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //sets the slider text to show how many rounds has been selected
         sliderText.text = ""+ roundsSlider.value;
     }
 
+    //function called when player clicks 2 players
     public void TwoPlayer()
     {
+        //sets the amount of players playing to 2
         PlayerPrefs.SetInt("Players", 2);
     }
-    public void ThreePlayer()
-    {
-        PlayerPrefs.SetInt("Players", 3);
+    //function called when player clicks 3 players
 
+    public void ThreePlayer()
+    {   
+        //sets the amount of players playing to 3
+        PlayerPrefs.SetInt("Players", 3);
     }
+    //function called when player clicks 4 players
     public void FourPlayer()
     {
+        //sets the amount of players playing to 4
         PlayerPrefs.SetInt("Players", 4);
-
     }
 
+    //function called when player clicks the Play button
     public void Play()
     {
+        //set the number of rounds to be the selected amount
         PlayerPrefs.SetInt("NoOfRounds", Mathf.RoundToInt(roundsSlider.value) + 1);
+        //loads the game pick menu
         SceneManager.LoadScene("GamePick");
     }
 }
