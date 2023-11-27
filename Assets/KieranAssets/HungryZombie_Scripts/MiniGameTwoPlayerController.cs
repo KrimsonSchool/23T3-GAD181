23fb754e1cf377c4dcfcd01ecd04305a7bc58bce
TORBJORNGAMINGP\Windows PC <1027606@student.sae.edu.au>
2023-11-27 21:04:07 +1000

    Not sure how this work to be honest

--- Assets/KieranAssets/HungryZombie_Scripts/MiniGameTwoPlayerController.cs
+++ Assets/KieranAssets/HungryZombie_Scripts/MiniGameTwoPlayerController.cs
@@ -28,7 +28,7 @@ public class MiniGameTwoPlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
