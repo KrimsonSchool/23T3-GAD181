4a12fc6d060d09641401e03d183449a646ceaa02
SAE1\1027859 <1027859@student.sae.edu.au>
2023-11-27 12:24:27 +1000

    UI Stuff + cool zombie Ai that wasnt wanted  =(

--- Assets/JoshAssets/Script/CureMaker/ZombieRoam.cs
+++ Assets/JoshAssets/Script/CureMaker/ZombieRoam.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,17 +8,15 @@ public class ZombieRoam : MonoBehaviour
 {
     // Start is called before the first frame update
     public NavMeshAgent agent;
+    NavMeshPath navMeshPath;
+    public float max;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.speed = 10;
-        agent.destination = new Vector3(transform.position.x, transform.position.y, transform.position.z -12);
-        if (Vector3.Distance(transform.position, agent.destination) < 2)
-        {
-            agent.destination = new Vector3(transform.position.x + 6, transform.position.y, transform.position.z + 12);
-            agent.destination = new Vector3(transform.position.x - 6, transform.position.y, transform.position.z - 12);
-        }
+        PickDest();
+        navMeshPath = new NavMeshPath();
     }
 
 
@@ -28,7 +27,21 @@ public class ZombieRoam : MonoBehaviour
     void Update()
     {
                   
-       
+       if(Vector3.Distance(transform.position, agent.destination) < 2)
+        {
+            PickDest();
+        }
 
+        agent.CalculatePath(agent.destination, navMeshPath);
+        if (navMeshPath.status != NavMeshPathStatus.PathComplete)
+        {
+            PickDest();
+        }
+
+    }
+
+    public void PickDest()
+    {
+        agent.destination = new Vector3(transform.position.x+Random.Range(-max, max), transform.position.y, transform.position.z + Random.Range(-max, max));
     }
 }
