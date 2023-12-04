6e96e89e3e825962e57d59d90cc168717030911a
SAE1\1027859 <1027859@student.sae.edu.au>
2023-12-04 14:07:23 +1000

    Can Customise name switch colours

--- Assets/CreditsTextModulatorMajig.cs
+++ Assets/CreditsTextModulatorMajig.cs
@@ -7,25 +7,51 @@ public class CreditsTextModulatorMajig : MonoBehaviour
     public TMPro.TextMeshProUGUI creditsText;
     public string[] names;
     public Color[] colours;
+    public Color[] coloursB;
     string[] clrs;
+    public float changePeriod;
+    bool crnt;
+    float tmr;
     // Start is called before the first frame update
     void Start()
     {
         clrs = new string[colours.Length];
-
-        for (int i = 0; i < colours.Length; i++)
-        {
-            clrs[i] = ColorUtility.ToHtmlStringRGB(colours[i]);
-        }
+        colourChange();
     }
 
     // Update is called once per frame
     void Update()
     {
+        tmr += Time.deltaTime;
+
+        if(tmr > changePeriod)
+        {
+            colourChange();
+            tmr = 0;
+        }
+    }
+
+    public void colourChange()
+    {
+        for (int i = 0; i < colours.Length; i++)
+        {
+            if (crnt)
+            {
+                clrs[i] = ColorUtility.ToHtmlStringRGB(coloursB[i]);
+            }
+            else
+            {
+                clrs[i] = ColorUtility.ToHtmlStringRGB(colours[i]);
+
+            }
+        }
+
         creditsText.text = "<color=red> Problematic Times </color> © 2023 | Created by ";
-        for (int i = 0;i < names.Length;i++)
+        for (int i = 0; i < names.Length; i++)
         {
             creditsText.text += "<color=#" + clrs[i] + ">" + names[i] + "</color>, ";
         }
+
+        crnt=!crnt;
     }
 }
